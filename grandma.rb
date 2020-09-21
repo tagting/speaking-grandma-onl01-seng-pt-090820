d325d134048324af793f798b6bc496f262dfe4ce
Ting Ting Zheng <ttzhenng@gmail.com>
2020-09-21 00:43:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,9 @@
-phrase = gets.chomp
-
-if phrase == "I LOVE YOU GRANDMA"
-  puts "I LOVE YOU TOO PUMPKIN"
-
-elsif phrase == phrase.upcase
-  puts "NO, NOT SINCE 1938!"
-
-else
-  puts "HUH?! SPEAK UP, SONNY!"
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA"
+    puts "I LOVE YOU TOO PUMPKIN"
+  elsif phrase == phrase.upcase
+    puts "NO, NOT SINCE 1938!"
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
   
 end
