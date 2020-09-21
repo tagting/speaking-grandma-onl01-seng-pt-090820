2d4f2f7ad07d940b96f5e96f4bde0e5677d1fd7e
Ting Ting Zheng <ttzhenng@gmail.com>
2020-09-21 17:31:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
-    return "HUH?! SPEAK UP, SONNY!."
+    return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
+  else
     return "I LOVE YOU TOO PUMPKIN!"
   end
 end
